87b9e052cedfd332be42d42e48030e690f30f15f
krushika Polra <kpolara548@gmail.com>
2023-09-27 10:08:34 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'view/home/home_view.dart';
+import 'view/storage_view/Storage_view_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeView(),
+      home: const StorageView(),
     );
   }
 }
---
+++ lib/view/storage_view/Storage_view_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_todo_app/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class StorageView extends StatefulWidget {
+  const StorageView({super.key});
+
+  @override
+  State<StorageView> createState() => _StorageViewState();
+}
+
+class _StorageViewState extends State<StorageView> {
+  final ImagePicker picker = ImagePicker();
+  XFile? image;
+
+  FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+
+  selectImage() async {
+    image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      debugPrint("Image path ------>> ${image!.path}");
+      sendImage();
+    }
+  }
+
+  sendImage() async {
+    try {
+      Reference reference = firebaseStorage.ref().child("image/123/1234567");
+      await reference.putFile(File(image!.path)).then((value) async {
+        var link = await reference.getDownloadURL();
+        debugPrint("Download Link ----> $link ");
+      });
+
+      var link = await reference.getDownloadURL();
+      debugPrint("Download link ----> $link");
+    } on FirebaseException catch (error) {
+      debugPrint("Firebase error ----> $error");
+      Utils().showSnackBar(context: context, content: "Firebase error ----> $error");
+    } catch (error) {
+      Utils().showSnackBar(context: context, content: "Error ----> $error");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              selectImage();
+            },
+            child: const Text("Select Image"),
+          ),
+        ],
+      ),
+    );
+  }
+}
